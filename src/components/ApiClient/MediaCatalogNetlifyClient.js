fee6d720422bef68658834ded81b629fae4611c7
mnsrulz <manishrawat4u@gmail.com>
2021-02-28 15:36:42 -0500

    axios auth headers added via interceptors

--- src/components/ApiClient/MediaCatalogNetlifyClient.js
+++ src/components/ApiClient/MediaCatalogNetlifyClient.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 function AuthenticatedClient() {
-    const idToken = JSON.parse(localStorage.token).tokenId;
-    return axios.create({
-        baseURL: 'https://mediacatalog.netlify.app/.netlify/functions/server/',
-        headers: {
-            Authorization: 'Bearer ' + idToken
-        }
+    var _instance = axios.create({
+        baseURL: 'https://mediacatalog.netlify.app/.netlify/functions/server/'
     });
+    _instance.interceptors.request.use(config => {        
+        const idToken = JSON.parse(localStorage.token).tokenId;
+        config.headers.Authorization = 'Bearer ' + idToken;
+        return config;
+    });
+    return _instance;
 }
 
 export const apiClient = AuthenticatedClient();
